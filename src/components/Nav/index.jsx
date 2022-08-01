05da70a052956b031a04374f19395ac2908f1242
parasSolanki <parassolanki151@gmail.com>
2022-08-01 21:40:22 +0530

    updated nav design

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,5 +1,4 @@
 import { h } from 'preact'
-import Styles from './styles.module.scss'
 import logo from '@/assets/logo.svg'
 
 const links = [
@@ -14,6 +13,13 @@ const links = [
   {
     name: 'Products',
     href: '/products',
+    childern: [
+      { name: 'Pillar / Driling Machine Column', href: '/pillar' },
+      {
+        name: 'Coupling / Rolling Mill Coupling Machine Column',
+        href: '/coupling',
+      },
+    ],
   },
   {
     name: 'Contact',
@@ -27,18 +33,36 @@ const links = [
 
 function Nav() {
   return (
-    <nav className={Styles.nav}>
-      <a className={Styles.logolink} href="/">
-        <div className="w-36">
-          <img src={logo} alt="logo" className="w-full h-full" />
+    <header className="h-24">
+      <div className="container  py-5 mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <a
+            href="/"
+            className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-black rounded"
+          >
+            <div className="w-36">
+              <img src={logo} alt="logo" className="w-full h-full" />
+            </div>
+          </a>
+          <nav>
+            <ul className="flex items-center space-x-6">
+              {links.map((link) => (
+                <li>
+                  <a
+                    className="text-base text-slate-600 hover:text-sky-400 focus:text-sky-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-sky-400 rounded"
+                    href={link.href}
+                    title={link.name}
+                  >
+                    {link.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
-      </a>
-      {links.map((link) => (
-        <a className={Styles.link} href={link.href} title={link.name}>
-          {link.name}
-        </a>
-      ))}
-    </nav>
+      </div>
+    </header>
   )
 }
+
 export default Nav
