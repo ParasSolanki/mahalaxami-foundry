c1d0eb9f3d915c2629b82021648a23d880f75b30
parasSolanki <parassolanki151@gmail.com>
2022-08-02 21:13:24 +0530

    added nav link with dropdown, updated styling

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -12,7 +12,6 @@ const links = [
   },
   {
     name: 'Products',
-    href: '/products',
     childern: [
       { name: 'Pillar / Driling Machine Column', href: '/pillar' },
       {
@@ -31,6 +30,49 @@ const links = [
   },
 ]
 
+const ChevronDownIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-5 w-5"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+  </svg>
+)
+
+const LinkWithDropdown = ({ link }) => (
+  <li className="dropdown">
+    <label
+      tabindex="0"
+      className="inline-flex items-center text-base text-slate-600 cursor-pointer hover:text-sky-400 focus:text-sky-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-sky-400 rounded"
+    >
+      {link.name}
+      <span className="ml-1.5">
+        <ChevronDownIcon />
+      </span>
+    </label>
+    <ul
+      tabindex="0"
+      className="dropdown-content menu p-2 shadow w-64 bg-white mt-5 rounded-md"
+    >
+      {link.childern.map((linkItem) => (
+        <li>
+          <a
+            className="text-sm focus:bg-sky-400 focus:text-white"
+            href={linkItem.href}
+            title={linkItem.name}
+          >
+            {linkItem.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </li>
+)
+
 function Nav() {
   return (
     <header className="h-24">
@@ -46,17 +88,21 @@ function Nav() {
           </a>
           <nav>
             <ul className="flex items-center space-x-6">
-              {links.map((link) => (
-                <li>
-                  <a
-                    className="text-base text-slate-600 hover:text-sky-400 focus:text-sky-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-sky-400 rounded"
-                    href={link.href}
-                    title={link.name}
-                  >
-                    {link.name}
-                  </a>
-                </li>
-              ))}
+              {links.map((link) => {
+                return link?.childern ? (
+                  <LinkWithDropdown link={link} />
+                ) : (
+                  <li>
+                    <a
+                      className="text-base text-slate-600 hover:text-sky-400 focus:text-sky-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-sky-400 rounded"
+                      href={link.href}
+                      title={link.name}
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                )
+              })}
             </ul>
           </nav>
         </div>
