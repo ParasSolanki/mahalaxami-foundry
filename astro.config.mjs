b91b5e470acd5b4a9a2c5006d56581d68109f07e
parasSolanki <parassolanki151@gmail.com>
2022-08-12 18:53:12 +0530

    added contact and enquiry page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
 
+const baseUrl =
+  process.env.NODE_ENV === 'production' ? 'mahalaxami-foundry/' : '/'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   site: 'https://parassolanki.github.io',
-  base: 'mahalaxami-foundry/',
+  base: baseUrl,
 })
