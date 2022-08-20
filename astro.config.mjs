77b0dd2f9f03d3c7ec0f6b6020c82123a38bd03b
parasSolanki <parassolanki151@gmail.com>
2022-08-20 13:19:17 +0530

    bump up astro to v1 also preact, tailwind versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
-
 const baseUrl =
   process.env.NODE_ENV === 'production' ? 'mahalaxami-foundry/' : '/'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [preact(), tailwind()],
   site: 'https://parassolanki.github.io',
   base: baseUrl,
 })
