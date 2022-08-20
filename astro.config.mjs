46dd920fabea998760e36b224e58eb1cd8add30e
parasSolanki <parassolanki151@gmail.com>
2022-08-20 14:02:11 +0530

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
 
-const baseUrl = process.env.MODE === 'production' ? '/mahalaxami-foundry' : '/'
+const baseUrl =
+  import.meta.env.MODE === 'production' ? '/mahalaxami-foundry' : '/'
 
 // https://astro.build/config
 export default defineConfig({
