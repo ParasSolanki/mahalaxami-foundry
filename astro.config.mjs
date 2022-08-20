7085baafaea7f274102cb079cc71775f1f796531
parasSolanki <parassolanki151@gmail.com>
2022-08-20 22:08:05 +0530

    added mdx integration, tailwind typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
+  integrations: [mdx(), preact(), tailwind()],
   site: 'https://parassolanki.github.io',
   base: '/mahalaxami-foundry',
 })
