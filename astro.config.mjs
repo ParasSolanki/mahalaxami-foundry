ae873c79917acd71b69256b2525f857fb40296cf
parasSolanki <parassolanki151@gmail.com>
2022-09-11 21:25:48 +0530

    add: assets, show images in grid view

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-
-import image from "@astrojs/image";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), react(), image()],
   site: 'https://parassolanki.github.io',
-  base: ''
-});
+  base: '/mahalaxami-foundry',
+})
